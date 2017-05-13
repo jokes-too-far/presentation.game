db8dd62a7ead52b0d1d606cc1b7678bcc1d4d2f2
Evan <evan@projectpixelpress.com>
2017-05-13 18:08:34 -0500

    basic random words 'works', not happy with it

--- src/contentGeneration/themes.js
+++ src/contentGeneration/themes.js
@@ -1,12 +1,28 @@
 const dictionary = {
-  'ducks': {},
-  'synergy': {},
-  'robots': {},
-  'the economy': {},
-  '12th century spanish literature': {},
-  'aliens': {},
-  'a well-rounded diet': {},
-  'cat ears': {},
+  'ducks': {
+    'synonyms': ['quackers', 'quack', 'mallard', 'delicious, smoked duck', 'eggs', 'darkwing duck', 'venomous?', 'decoy', 'breasting out', 'keel', 'waterfowl'],
+  },
+  'synergy': {
+    'synonyms': ['synergism', 'action', 'activity', 'pecking order', 'collaboration', 'paradigm shifts', 'key performance indicators', 'outside the box', 'be proactive', 'globalized diversity', 'eat your own dogfood', 'holistic approach', 'embrace millennials', 'leverage generation x', 'higher-order thinking', 'algorithmically enhanced', 'fully digital'],
+  },
+  'robots': {
+    'synonyms': ['automaton', 'golem', 'droid', 'replicants', 'skynet', 'are they already among you?', 'can they replace your girlfriend?', 'error-prone', 'redundant', 'well-oiled machines', 'replace flimsy humans', 'repetitive tasks'],
+  },
+  'the economy': {
+    'synonyms': ['trickle down economics', 'fiscally sound', 'balanced budget', 'bank panic', 'fiscal new year', 'budget surplus', 'demand-pull inflation', 'a new great depression', 'foreign exchange moguls', 'the invisible hand', 'free enterprise', 'Laissez-faire', 'mandatory spending', 'scarcity'],
+  },
+  // '12th century spanish literature': {
+  //   'synonyms': [],
+  // },
+  // 'aliens': {
+  //   'synonyms': [],
+  // },
+  // 'a well-rounded diet': {
+  //   'synonyms': [],
+  // },
+  // 'cat ears': {
+  //   'synonyms': [],
+  // },
 };
 const themes = Object.keys(dictionary);
 
