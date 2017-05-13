72592cc4f423a594f277b1ef9330de8162307633
Evan <evan@projectpixelpress.com>
2017-05-13 15:06:02 -0500

    extracting title generator

--- src/states/menu.js
+++ src/states/menu.js
@@ -1,3 +1,5 @@
+import titleGenerator from '../titleGenerator'
+
 class Menu extends Phaser.State {
 
   constructor() {
@@ -5,20 +7,8 @@ class Menu extends Phaser.State {
   }
 
   create() {
-    var grammar = tracery.createGrammar({
-      'presentation': ['presentment',' demonstration',' display',' introduction',' intro',' attitude',' ceremony',' informing',' making known',' position',' posture',' proposal',' proposition',' representation', 'show'],
-      'on': ['on', 'about', 'regarding'],
-      'theme': ['ducks', 'synergy', 'robots', 'the future', 'why our economy is failing', '12th century spanish literature'],
-      'generated-title':['#presentation.a# #on# #theme#'],
-    });
 
-    grammar.addModifiers(tracery.baseEngModifiers);
-
-    const output = grammar.flatten('#generated-title#');
-    console.log(output);
-
-
-    var text = this.add.text(this.game.width * 0.5, this.game.height * 0.5, toTitleCase(output), {
+    var text = this.add.text(this.game.width * 0.5, this.game.height * 0.5, titleGenerator(), {
       font: '42px Arial',
       fill: '#ffffff',
       align: 'center'
@@ -34,11 +24,6 @@ class Menu extends Phaser.State {
     this.game.state.start('game');
   }
 
-}
-
-function toTitleCase(str)
-{
-    return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
 }
 
 export default Menu;
