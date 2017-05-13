03a6b90e01630777f9e61065182ce5dffdcce8e3
Evan <evan@projectpixelpress.com>
2017-05-13 16:27:22 -0500

    fixing a/an in titles, making them more coherent, adding more titles

--- src/titleGenerator.js
+++ src/titleGenerator.js
@@ -1,9 +1,12 @@
 const generateTitle = () => {
   var grammar = tracery.createGrammar({
-    'presentation': ['presentment',' demonstration',' display',' introduction',' intro',' attitude',' ceremony',' informing',' making known',' position',' posture',' proposal',' proposition',' representation', 'show'],
-    'on': ['on', 'about', 'regarding'],
-    'theme': ['ducks', 'synergy', 'robots', 'the future', 'why our economy is failing', '12th century spanish literature'],
-    'generated-title':['#presentation.a# #on# #theme#'],
+    'presentation': ['presentment', 'demonstration', 'display', 'introduction', 'intro', 'attitude', 'ceremony', 'informing', 'making known', 'position', 'posture', 'proposal', 'proposition', 'representation', 'show'],
+    'on': ['on', 'about', 'regarding', 'concerning'],
+    'importance': ['the future of', 'the importance of', 'the uselessness of'],
+    'theme': ['ducks', 'synergy', 'robots', 'the economy', '12th century spanish literature', 'aliens', 'a well-rounded diet', 'cat ears'],
+    'ridiculous-claim': ['will save your life', 'is ruining the future', 'is the future', 'will change everything'],
+    'question': ['why', 'how'],
+    'generated-title': ['#presentation.a# #on# #importance# #theme#', '#question# #theme# #ridiculous-claim#'],
   });
 
   grammar.addModifiers(tracery.baseEngModifiers);
@@ -14,7 +17,9 @@ const generateTitle = () => {
 }
 
 const toTitleCase = (str) => {
-    return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+  return str.replace(/\w\S*/g, function(txt) {
+    return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+  });
 }
 
 module.exports = generateTitle;
