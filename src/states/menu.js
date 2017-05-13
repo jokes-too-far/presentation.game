30cc60da62bc64c04a4518ea4807dc7c25571df8
Evan <evan@projectpixelpress.com>
2017-05-13 14:50:39 -0500

    tracery pulls in (with errors)

--- src/states/menu.js
+++ src/states/menu.js
@@ -3,10 +3,26 @@ class Menu extends Phaser.State {
   constructor() {
     super();
   }
-  
+
   create() {
-    var text = this.add.text(this.game.width * 0.5, this.game.height * 0.5, 'MENU', {
-      font: '42px Arial', fill: '#ffffff', align: 'center'
+
+
+    var grammar = tracery.createGrammar({
+      'animal': ['panda','fox','capybara','iguana'],
+      'emotion': ['sad','happy','angry','jealous'],
+      'origin':['I am #emotion.a# #animal#.'],
+    });
+
+    grammar.addModifiers(tracery.baseEngModifiers);
+
+    const output = grammar.flatten('#origin#');
+    console.log(output);
+
+
+    var text = this.add.text(this.game.width * 0.5, this.game.height * 0.5, output, {
+      font: '42px Arial',
+      fill: '#ffffff',
+      align: 'center'
     });
     text.anchor.set(0.5);
 
@@ -15,7 +31,7 @@ class Menu extends Phaser.State {
 
   update() {}
 
-  startGame () {
+  startGame() {
     this.game.state.start('game');
   }
 
