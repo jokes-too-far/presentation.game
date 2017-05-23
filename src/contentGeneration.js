78349c8ceefb62a6b72c5fddaee91e10a5ce6fee
Evan Cobb <cobb.evan@gmail.com>
2017-05-22 23:16:50 -0500

    fixing title tense, fleshing out themes more

--- src/contentGeneration.js
+++ src/contentGeneration.js
@@ -2,20 +2,14 @@ import CenteredContent from './prefabs/centeredContent'
 
 const dictionary = {
   'ducks': {
-    'synonyms': ['quackers', 'quack', 'mallard', 'delicious, smoked duck', 'eggs', 'darkwing duck', 'venomous?', 'decoy', 'breasting out', 'keel', 'waterfowl'],
+    'synonyms': ['totally quackers', 'they quack', 'do not confuse with geese\n(especially canadian)', '<PRESENTER IMITATES DUCK CALL FOR REMAINDER OF SLIDE>',  'delicious, smoked duck', 'the taste of their eggs', 'darkwing duck', 'are they truly venomous?', 'actually a duck, or a decoy?', '<DEMONSTRATE BREASTING OUT>', '<SHOW THEM WHERE A DUCK\'S KEEL IS>', 'Examples of Fowl play', 'the difference between you and a mallard with a cold?', '<PRESENT LIST OF DUCK PUNS NOW>'],
   },
-  'synergy': {
-    'synonyms': ['synergism', 'action', 'activity', 'pecking order', 'collaboration', 'paradigm shifts', 'key performance indicators', 'outside the box', 'be proactive', 'globalized diversity', 'eat your own dogfood', 'holistic approach', 'embrace millennials', 'leverage generation x', 'higher-order thinking', 'algorithmically enhanced', 'fully digital'],
+  'corporate strategies': {
+    'synonyms': ['synergism is winnerism', 'always take action items', 'maintain peak activity', 'revere the pecking order', 'collaboration = coordination = condensation', 'shift your paradigm to a new dimension', 'key performance indicators and how they rule you now', 'WORK outside the box', 'Thinkle outside the binkle', 'be proactive', 'globalized diversity to diversify globally', 'eat your own dogfood\n(dogfooding)', 'take a holistic approach', 'embrace millennials and their lack of money', 'leverage generation x', 'higher-order thinking', 'algorithmically enhance your data', 'fully digital is the way of the future', '1. pitch idea\n2. ???\n3. profit!'],
   },
   'robots': {
-    'synonyms': ['automaton', 'golem', 'droid', 'replicants', 'skynet', 'are they already among you?', 'can they replace your girlfriend?', 'error-prone', 'redundant', 'well-oiled machines', 'replace flimsy humans', 'repetitive tasks'],
+    'synonyms': ['automatically automated automaton autocracies', 'golems\n\n (or gollums if you\'re a nerd)', 'BEEP BOOP BEEP BOOP', '<say "SNEEP SNOOP" while holding nose>', 'do not fear skynet', 'they are already among you', 'There is one sitting next to you right now', 'can they replace your significant other?', 'infinitely upgradeable unlike traditional meatbags', 'replace flimsy, fleshy humans', 'perform repetitive tasks with astounding efficiency'],
   },
-  'the economy': {
-    'synonyms': ['trickle down economics', 'fiscally sound', 'balanced budget', 'bank panic', 'fiscal new year', 'budget surplus', 'demand-pull inflation', 'a new great depression', 'foreign exchange moguls', 'the invisible hand', 'free enterprise', 'Laissez-faire', 'mandatory spending', 'scarcity'],
-  },
-  // '12th century spanish literature': {
-  //   'synonyms': [],
-  // },
   // 'aliens': {
   //   'synonyms': [],
   // },
@@ -24,6 +18,9 @@ const dictionary = {
   // },
   // 'cat ears': {
   //   'synonyms': [],
+  // },
+  // 'the economy': {
+  //   'synonyms': ['trickle down economics', 'fiscally sound', 'balanced budget', 'bank panic', 'fiscal new year', 'budget surplus', 'demand-pull inflation', 'a new great depression', 'foreign exchange moguls', 'the invisible hand', 'free enterprise', 'Laissez-faire', 'mandatory spending', 'scarcity'],
   // },
 };
 const themes = Object.keys(dictionary);
@@ -45,13 +42,14 @@ const makeSlides = (game, theme) => {
 
 const generateTitle = (theme) => {
   var grammar = tracery.createGrammar({
-    'presentation': ['presentment', 'demonstration', 'display', 'introduction', 'intro', 'attitude', 'ceremony', 'informing', 'making known', 'position', 'posture', 'proposal', 'proposition', 'representation', 'show'],
+    'presentation': ['presentment', 'demonstration', 'display', 'introduction', 'ceremony', 'informing', 'making known', 'defense', 'proposal', 'proposition', 'survey'],
     'on': ['on', 'about', 'regarding', 'concerning'],
     'importance': ['the future of', 'the importance of', 'the uselessness of'],
     'theme': theme,
-    'ridiculous-claim': ['will save your life', 'is ruining the future', 'is the future', 'will change everything'],
+    'ridiculous-claim': ['saved my life', 'will ruin the future', 'will change everything', 'are so 1999'],
     'question': ['why', 'how'],
-    'generated-title': ['#presentation.a# #on# #importance# #theme#', '#question# #theme# #ridiculous-claim#'],
+    'superlative': ['amazing', 'unbelievable', '<INSERT SUPERLATIVE>', 'astounding', 'life-changing'],
+    'generated-title': ['#presentation.a# #on# #importance# #theme#', '#question# #theme# #ridiculous-claim#', '10 Amazing slides about #theme#', '#theme# and you\n #presentation.a##'],
   });
 
   grammar.addModifiers(tracery.baseEngModifiers);
