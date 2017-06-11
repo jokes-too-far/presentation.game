79443c705582c806494e70d9f119b293a9c8f642
rasbury <rasbury7@gmail.com>
2017-06-11 13:50:12 -0500

    fixed bonus slides not happening
    also added more feedback questions

--- src/states/menu.js
+++ src/states/menu.js
@@ -10,12 +10,18 @@ class Menu extends Phaser.State {
     super();
   }
 
-  create() {    
+  create() {
     styles.backgroundColor(this.game);
     new GradientBG(this.game);
-  
+
     const theme = contentGeneration.pickTheme();
     this.game.global.title = contentGeneration.generateTitle(this.game, theme);
+    if (Math.random() < (this.game.global.bonusSlideChancePercent/100)){
+      this.game.global.addBonusSlide = 1;
+    }else{
+      this.game.global.addBonusSlide = 0;
+    };
+
     const slides = contentGeneration.makeSlides(this.game, theme);
     new CenteredContent(this.game, this.game.global.title);
 
