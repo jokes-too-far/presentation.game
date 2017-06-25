978fa491bc73e33adb6502f3328dcb02e04f84e8
Evan <evan@projectpixelpress.com>
2017-06-25 16:13:00 -0500

    extracting global config to its own file

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import Menu from './states/menu';
 import Preloader from './states/preloader';
 import Gameover from './states/gameover';
 import Feedback from './states/feedback';
+import globalConfig from './globalConfig'
 
 var game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.AUTO, 'presentation.game-game');
 game.state.add('boot', new Boot());
@@ -12,5 +13,7 @@ game.state.add('menu', new Menu());
 game.state.add('preloader', new Preloader());
 game.state.add('gameover', new Gameover());
 game.state.add('feedback', new Feedback());
+
+game.global = globalConfig;
 
 game.state.start('boot');
