a100abb477595c460197852530e18852ca501049
Evan Cobb <cobb.evan@gmail.com>
2017-07-08 23:32:54 -0500

    removing dead, misleading code in feedback state

--- src/states/feedback.js
+++ src/states/feedback.js
@@ -4,34 +4,18 @@ const CenteredContent = require('../prefabs/centeredContent')
 
 class Menu extends Phaser.State {
 
-  constructor() {
-    super();
-  }
-
   create() {
     new GradientBG(this.game);
 
     const feedbackText = contentGeneration.makeFeedbackSlide();
     new CenteredContent(this.game, feedbackText);
 
-    this.saveVarsToLocalStorage();
-
     this.input.onDown.add(this.restartGame, this);
   }
-
-  saveVarsToLocalStorage(){
-
-  }
-
-  resetGlobalVariables(){
-
-  }
-
 
   update() {}
 
   restartGame () {
-    this.resetGlobalVariables();
     this.game.state.start('menu');
   }
 
