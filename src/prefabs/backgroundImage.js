d5f698a240cbd2bedd6c920323b0e40e5a736df1
Evan Cobb <cobb.evan@gmail.com>
2018-01-02 19:55:31 -0600

    bg image maxes its size out and scales well

--- src/prefabs/backgroundImage.js
+++ src/prefabs/backgroundImage.js
@@ -4,6 +4,11 @@ class BackgroundImage extends Phaser.Sprite {
         const key = game.rnd.pick(game.global.bgs);
         super(game, game.world.centerX, game.world.centerY, key);
         this.anchor.setTo(0.5);
+        const widthRatio = game.width / this.width;
+        const heightRatio = game.height / this.height;
+        const ratio = Math.max(heightRatio, widthRatio);
+        this.width = this.width * ratio;
+        this.height = this.height * ratio;
         game.add.existing(this);
     }
 
